d615e6f40dabd753fb7f604f0ff5cf2e5270a37f
Olugbenga Odedele <odedeleg@gmail.com>
2022-04-11 15:36:15 -0500

    setup environment, setup migration for users table

---
+++ src/database.ts
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+let client: Pool;
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_TEST_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  NODE_ENV,
+} = process.env;
+
+if (NODE_ENV === "dev") {
+  console.log("I am in dev mode");
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+} else if (NODE_ENV === "test") {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+} else {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_HOST,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+
+export default client;
