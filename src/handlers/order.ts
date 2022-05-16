5695f320d166f21734f25925e638c297399946d9
Olugbenga Odedele <odedeleg@gmail.com>
2022-05-15 19:02:47 -0500

    added the orders handler file

---
+++ src/handlers/order.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from "express";
+import { Order, OrderStore, OrderProducts } from "../models/order";
+import { verifyAuthToken } from "./user";
+
+const store = new OrderStore();
+
+const index = async (req: Request, res: Response) => {
+  try {
+    const orders = await store.index();
+    res.json(orders);
+  } catch (error) {
+    res.status(400);
+    res.json("Cannot fetch all orders");
+  }
+};
+
+const show = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const order = await store.show(id);
+    res.json(order);
+  } catch (error) {
+    res.status(400);
+    res.json("Cannot get order");
+  }
+};
+
+const create = async (req: Request, res: Response) => {
+  try {
+    const order: Order = req.body;
+    const newOrder = await store.create(order);
+    res.json(newOrder);
+  } catch (error) {
+    res.status(400);
+    res.json("Cannot create new order");
+  }
+};
+
+const addProducts = async (req: Request, res: Response) => {
+  const orderId = req.params.id;
+  const productId = req.body.productId;
+  const quantity = req.body.quantity;
+  try {
+    const addNewProduct = await store.addProducts(productId, orderId, quantity);
+    res.json(addNewProduct);
+  } catch (error) {
+    res.status(400);
+    res.json("Cannot create new order");
+  }
+};
+
+const order_routes = (app: express.Application) => {
+  app.get("/orders", index);
+  app.post("/orders", verifyAuthToken, create);
+  app.get("/orders/:id", show);
+  app.post("/orders/:orderId/products", verifyAuthToken, addProducts);
+};
+
+export default order_routes;
