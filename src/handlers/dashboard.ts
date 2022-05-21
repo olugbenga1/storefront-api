89f4b4debde839cb0ee045a032de962a7dd036a0
Olugbenga Odedele <odedeleg@gmail.com>
2022-05-20 21:34:56 -0500

    I added the dashboard handler file database.ts

---
+++ src/handlers/dashboard.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import { DashboardQueries } from "../services/dashboard";
+
+const dashboard = new DashboardQueries();
+
+const productsInOrders = async (req: Request, res: Response) => {
+  const products = await dashboard.productsInOrders();
+  res.json(products);
+};
+
+const dashboardRoutes = (app: express.Application) => {
+  app.get("/products_in_orders", productsInOrders);
+};
+
+export default dashboardRoutes;
