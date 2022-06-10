90c15f771dafa8487f4c2b51de38e9eb1a6e9a4d
Olugbenga Odedele <odedeleg@gmail.com>
2022-06-09 22:58:49 -0500

    updated storefront test suite

--- src/models/tests/storefront_spec.ts
+++ src/models/tests/storefront_spec.ts
@@ -1,13 +1,37 @@
 import { User, UserStore } from "../user";
+import { Product, ProductStore } from "../product";
+import { Order, OrderStore } from "../order";
 
-const store = new UserStore();
+const user = new UserStore();
+const product = new ProductStore();
+const order = new OrderStore();
 
-describe("Storefront model", () => {
+describe("user model", () => {
   it("should have an index method", () => {
-    expect(store.index).toBeDefined();
+    expect(user.index).toBeDefined();
+  });
+  it("index should return an array of users", async () => {
+    const result = await user.index();
+    expect(result).toEqual([]);
+  });
+});
+
+describe("product model", () => {
+  it("should have an index method", () => {
+    expect(product.index).toBeDefined();
   });
   it("index should return an array of products", async () => {
-    const result = await store.index();
+    const result = await product.index();
+    expect(result).toEqual([]);
+  });
+});
+
+describe("order model", () => {
+  it("should have an index method", () => {
+    expect(order.index).toBeDefined();
+  });
+  it("index should return an array of products", async () => {
+    const result = await order.index();
     expect(result).toEqual([]);
   });
 });
